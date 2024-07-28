2f0effdee0ed0d71fdb278c7e666b96fe5563507
Klimenty <klimentykk@gmail.com>
2024-07-28 15:49:41 +0300

    removed .build folder

--- .build/main.js
+++
@@ -1,5 +0,0 @@
-import { Bot } from "grammy";
-import "dotenv/config";
-var bot = new Bot(process.env.BOT_TOKEN);
-bot.on("message:text", function (ctx) { return ctx.reply(ctx.message.text); });
-bot.start();
