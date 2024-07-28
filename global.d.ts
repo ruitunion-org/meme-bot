90cacca1afeabf9482593873f795d241f81bcb3d
Klimenty <klimentykk@gmail.com>
2024-07-28 15:53:19 +0300

    augment global types

--- global.d.ts
+++ global.d.ts
@@ -3,6 +3,9 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: "development" | "production" | "test";
       BOT_TOKEN: string;
+      GROUP_ID_TO_TAKE_MEMES_FROM: string;
+      GROUP_ID_TO_POST_SUGGESTIONS: string;
+      TOPIC_ID_TO_SUBSCRIBE_TO_REACTIONS_FROM: string;
     }
   }
 }
